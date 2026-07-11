774e307111132c49776ef324dd1d46c621cc7436
Sreelaya1619 <162485808+Sreelaya1619@users.noreply.github.com>
2026-07-11 20:01:31 +0530

    Update title in Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -31,7 +31,7 @@ export default function Navbar({ darkMode, toggleDarkMode }) {
 
             <div>
               <h1 className="text-white font-bold text-lg">
-                  SREELAYA NEW STUDENT TASK MANAGER
+                  EXYRA STUDENT TASK MANAGER
               </h1>
               <p className="text-xs text-gray-300">
                 Stay Organized
